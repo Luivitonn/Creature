ab9eeccfed128f255b29289f3333743cc2305957
Luivitonn <160291067+Luivitonn@users.noreply.github.com>
2024-02-18 19:23:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const client = require('./Handlers/clientHandler')
 const Luicik = require('./Handlers/loaderHandler')
 require('./Handlers/functionHandler')
-const keep_alive = require('./keep_alive.js')
 process.on('uncaughtException', console.log)
 process.on('unhandledRejection', console.log)
 
 Luicik.connect()
 Luicik.fetchCommands()
 Luicik.fetchEvents()
+
+
+const express = require('express');
+const app = express ();
+const port = 3000;
+
+app.get('/'), (req, res) => {
+
+res.sendStatus(200);
+}};
+
+app.listen(port, () => {
+  console.log("Sunucu ${port} numaralı bağlantı noktasına yürütülüyor.");
+  }};
